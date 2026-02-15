df20503dfe1d612c1a89362553270465692ef339
spiderasasen <spiderasasen@gmail.com>
2026-02-15 18:53:20 -0500

    beginening the render section of the code

--- js/react_replicate.js
+++ js/react_replicate.js
@@ -1,6 +1,10 @@
 //fetches the api and starts praying that everything works
 fetch("https://script.google.com/macros/s/AKfycbzL0q3bqZAHjgb7XGRltydzGtSqROkit0Q3J4jsyHmmzopF6qdf__TBdLdSbB_sG_A/exec")
-    .then(res => res.json())
+    .then(res =>
+    {
+        if (!res.ok) throw new Error("Failed to fetch script: " + res.statusText);
+        return res.json();
+    })
     .then(editors => {
         console.log(editors);
         renderEditors(editors);
