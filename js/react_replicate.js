78fb626df467b7f36de046416ec7d22865074c82
spiderasasen <spiderasasen@gmail.com>
2026-02-15 19:07:32 -0500

    made the "react" system. it should work for now

--- js/react_replicate.js
+++ js/react_replicate.js
@@ -10,3 +10,31 @@ fetch("https://script.google.com/macros/s/AKfycbzL0q3bqZAHjgb7XGRltydzGtSqROkit0
         renderEditors(editors);
     })
     .catch(err => console.log("Something went wrong", err));
+
+//redering in all the eidtors
+function renderEditors(editors) {
+    const container = document.getElementById("editors_container");
+
+    //for each editor, create a card
+    editors.forEach(editor => {
+        const card = document.createElement("div");
+
+        //creates the card div
+        card.innerHTML = `
+            <div class="editor-card">
+                <h3>${editor.Name}</h3>
+                <p>${editor.Description}</p>
+                <p><strong>Email: ${editor.Email}</strong></p>
+            </div>
+        `;
+
+        //grabs the info from local stroage to add it to the editor name, discription and emil
+        card.addEventListener("click", () => {
+            localStorage.setItem("selectedName", editor.Name);
+            localStorage.setItem("selectedEmail", editor.Email);
+        });
+
+        container.appendChild(card);
+    })
+
+}
