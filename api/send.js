a19e16c780562f60b3e3836ce4c43ae0df0edf64
spiderasasen <spiderasasen@gmail.com>
2026-02-19 08:53:36 -0500

    praying that the key is read

--- api/send.js
+++ api/send.js
@@ -5,8 +5,8 @@ export default function handler(req, res) {
     if (req.method === "POST") {
         const data = req.body;
         console.log("Received:", data);
-        return res.status(200).json({ message: "Success", received: data });
         console.log(apiKey)
+        return res.status(200).json({ message: "Success", received: data });
     }
 
     return res.status(405).json({ message: "Method not allowed" });
