3e6b8219e088cba4bc34a71e7867373c9d0c2882
spiderasasen <spiderasasen@gmail.com>
2026-02-18 21:40:56 -0500

    connected the forms to the website users can now send emails. to me. im a tester right now

--- js/editor_page.js
+++ js/editor_page.js
@@ -9,9 +9,29 @@ const body = localStorage.getItem("body");
 const name = localStorage.getItem("name");
 const email = localStorage.getItem("email");
 
+// getting the forms section
+const formURL = "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=bTIYuh9x4kqGgWEVSTp6UwZTuf1pk8tDqwbdQB_E9NNUOVVJMUFJN1ZWNkNQNjVMN0VTUFhVSlU2Ry4u"
+const data = new FormData();
+
 //button job
 button.addEventListener("click", () => {
-    alert(`We've sent your documents to Diaz: ddiaz11@elon.edu`)
+    //telling the user who they selected
+    alert(`We've sent your documents to Diaz: ddiaz11@elon.edu`);
+
+    //pasting the info to the form
+    data.append("name", name);
+    data.append("email", email);
+    data.append("header", header);
+    data.append("subheader", subheader);
+    data.append("media", media);
+    data.append("body", body);
+
+    fetch(formURL, {
+       method: "POST",
+       body: data,
+    });
+
+    //clearing the storage and sending the user back to the home page
     localStorage.clear();
     console.log(localStorage);
     window.location.href = "index.html";
