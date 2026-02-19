f922c84e0a10b49dac81f543efdb047984004561
spiderasasen <spiderasasen@gmail.com>
2026-02-19 09:52:00 -0500

    made the main body skelton

--- api/send.js
+++ api/send.js
@@ -3,9 +3,35 @@ const apiKey = process.env.RESEND_API_KEY;
 
 export default function handler(req, res) {
     if (req.method === "POST") {
+        //checking if the info was received
         const data = req.body;
         console.log("Received:", data);
-        return res.status(200).json({ message: "Success", received: data });
+
+        //making the email
+        const { name, email, header, subheader, body, media } = data;
+        const emailPayload = {
+            from: "onboarding@resend.dev",
+            to: "ddiaz11@elon.edu",
+            subject: `New Opinion Submission from ${name}`,
+            text:`
+                User: 
+                ${name}, ${email}
+                
+                Header for the Opinion Submission Article 
+                ${header},
+                
+                Subheader for the Opinion Submission Article 
+                ${subheader},
+                
+                Media ${name} would like to have included in the Article: 
+                ${media}
+                
+                Main Draft:
+                ${body}
+            `
+        }
+
+        return res.status(200).json({ message: "Email Sent!!"});
     }
 
     return res.status(405).json({ message: "Method not allowed" });
