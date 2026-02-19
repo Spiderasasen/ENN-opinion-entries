61994e5ee821054e8bf70ede5649d90b9ee889a7
spiderasasen <spiderasasen@gmail.com>
2026-02-19 08:02:31 -0500

    starting from something else

--- js/editor_page.js
+++ js/editor_page.js
@@ -9,27 +9,10 @@ const body = localStorage.getItem("body");
 const name = localStorage.getItem("name");
 const email = localStorage.getItem("email");
 
-// getting the forms section
-const formURL = "https://forms.cloud.microsoft/r/0zR72B8BX2"
-const data = new FormData();
-
 //button job
 button.addEventListener("click", () => {
     //telling the user who they selected
     alert(`We've sent your documents to Diaz: ddiaz11@elon.edu`);
-
-    //pasting the info to the form
-    data.append("ref2766886f134736b1ef77209e07ab8e", name);
-    data.append("r8660ce3e70024eecb1113610024644fa", email);
-    data.append("r0ede07dc5f834029835167ab08cb3b57", header);
-    data.append("r2fabf1bf790749b8bdc8229b406c18d3", subheader);
-    data.append("r05c51efa2d1f469393e6c99460cdfe02", media);
-    data.append("r2734d2c7944b49749b76267f98b2e980", body);
-
-    fetch(formURL, {
-       method: "POST",
-       body: data,
-    });
 
     //clearing the storage and sending the user back to the home page
     localStorage.clear();
