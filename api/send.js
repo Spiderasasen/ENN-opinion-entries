d198013bcf93633d7817719de4d2083390e42ea2
spiderasasen <spiderasasen@gmail.com>
2026-02-19 12:33:29 -0500

    praying

--- api/send.js
+++ api/send.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
 
         //if an error happens when the email is being sent
         if (!response.ok) {
-            return res.status(500).json({ error: result });
+            return res.status(500).json({message: "An Error has occurred", error: result });
         }
 
         return res.status(200).json({ message: "Email Sent!!", id: result.id});
